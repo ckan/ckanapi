e12a17c075d11f0e97fe416f11ea4358c151d8e9
Ian Ward <ian@excess.org>
2013-04-11 19:41:35 -0400

    ckanapi: docstrings!

--- ckanapi.py
+++ ckanapi.py
@@ -1,7 +1,26 @@
+"""
+ckanapi
+-------
+
+This module a thin wrapper around the CKAN's action API.
+
+It may be used from within a plugin or if only using RemoteCKAN
+it may be used separate from CKAN.
+"""
+
 import urllib2
 import json
 
 class CKANAPIError(Exception):
+    """
+    The error raised from RemoteCKAN.call_action when no other error
+    is recognized.
+
+    If importing CKAN source fails then new versions of ParameterError,
+    NotAuthorized, ValidationError, NotFound, ParameterError,
+    SearchQueryError and SearchError are created as subclasses of this
+    class so that they provide a helpful str() for tracebacks.
+    """
     def __str__(self):
         return repr(self.args)
 
@@ -43,6 +62,22 @@ except ImportError:
 
 
 class ActionShortcut(object):
+    """
+    ActionShortcut(foo).bar(baz=2) <=> foo.call_action('bar', {'baz':2})
+
+    An instance of this class is used as the .action attribute of
+    LocalCKAN and RemoteCKAN instances to provide a short way to call
+    actions, e.g::
+
+        demo = RemoteCKAN('http://demo.ckan.org')
+        pkg = demo.action.package_show(id='adur_district_spending')
+
+    instead of::
+
+        demo = RemoteCKAN('http://demo.ckan.org')
+        pkg = demo.call_action('package_show', {'id':'adur_district_spending'})
+
+    """
     def __init__(self, ckan):
         self._ckan = ckan
 
@@ -53,6 +88,15 @@ class ActionShortcut(object):
 
 
 class LocalCKAN(object):
+    """
+    An interface to calling actions with get_action() for CKAN plugins.
+
+    :param username: perform action as this user, defaults to the site user
+                     and stored as self.username
+    :param context: a default context dict to use when calling actions,
+                    stored as self.context with username added as its 'user'
+                    value
+    """
     def __init__(self, username=None, context=None):
         from ckan.logic import get_action
         self._get_action = get_action
@@ -68,6 +112,12 @@ class LocalCKAN(object):
         return user['name']
 
     def call_action(self, action, data_dict=None, context=None):
+        """
+        :param action: the action name, e.g. 'package_create'
+        :param data_dict: the dict to pass to the action, defaults to {}
+        :param context: an override for the context to use for this action,
+                        remember to include a 'user' when necessary
+        """
         if not data_dict:
             data_dict = []
         if context is None:
@@ -77,12 +127,30 @@ class LocalCKAN(object):
 
 
 class RemoteCKAN(object):
+    """
+    An interface to the the CKAN API actions on a remote CKAN instance.
+
+    :param address: the web address of the CKAN instance, e.g.
+                    'http://demo.ckan.org', stored as self.address
+    :param api_key: the API key to pass as an 'Authorization' header
+                    when actions are called, stored as self.api_key
+    """
     def __init__(self, address, api_key=None):
         self.address = address
         self.api_key = api_key
         self.action = ActionShortcut(self)
 
     def call_action(self, action, data_dict=None):
+        """
+        :param action: the action name, e.g. 'package_create'
+        :param data_dict: the dict to pass to the action as JSON,
+                          defaults to {}
+
+        This function parses the response from the server as JSON and
+        returns the decoded value.  When an error is returned this
+        function will convert it back to an exception that matches the
+        one the action function itself raised.
+        """
         if not data_dict:
             data_dict = {}
         data = json.dumps(data_dict)
