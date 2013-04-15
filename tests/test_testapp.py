c0ecf449e1a8211d88b31f0eb425ed4514b336c3
Ian Ward <ian@excess.org>
2013-04-15 13:42:22 -0400

    better TestAppCKAN tests

--- tests/test_testapp.py
+++ tests/test_testapp.py
@@ -1,18 +1,21 @@
 
-#from wsgiref.simple_server import make_server
 import json
 import ckanapi
 import unittest
 import paste.fixture
 
 def wsgi_app(environ, start_response):
-
     status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
+    headers = [('Content-type', 'application/json')]
+
+    path = environ['PATH_INFO']
+    if path == '/api/action/hello_world':
+        response = {'success': True, 'result': 'how are you?'}
+    elif path == '/api/action/invalid':
+        response = {'success': False, 'error': {'__type': 'Validation Error'}}
 
     start_response(status, headers)
-
-    return [json.dumps({'result': 'looks good'})]
+    return [json.dumps(response)]
 
 
 class TestTestAPPCKAN(unittest.TestCase):
@@ -20,8 +23,14 @@ class TestTestAPPCKAN(unittest.TestCase):
         self.test_app = paste.fixture.TestApp(wsgi_app)
         self.ckan = ckanapi.TestAppCKAN(self.test_app)
 
-    def test_once(self):
-        self.ckan.action.hello_world()
+    def test_simple(self):
+        self.assertEquals(
+            self.ckan.action.hello_world()['result'],
+            'how are you?')
+    def test_invalid(self):
+        self.assertRaises(
+            ckanapi.ValidationError,
+            self.ckan.action.invalid)
 
 
 if __name__ == '__main__':
