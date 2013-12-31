a06194b01496dd9453860aa7f0bc2cd49febe85b
Thomas Levine <occurrence@thomaslevine.com>
2013-12-31 21:38:23 +0000

    a specific test for package_list

--- tests/test_call.py
+++ tests/test_call.py
@@ -14,13 +14,18 @@ class TestCallAction(unittest.TestCase):
             ckanapi.CKANAPIError,
             ckanapi.RemoteCKAN('fake').call_action,
             'fake', {}, 'context not allowed')
-    
+
     def test_test_fail(self):
         self.assertRaises(
             ckanapi.CKANAPIError,
             ckanapi.TestAppCKAN('fake').call_action,
             'fake', {}, 'context not allowed')
 
+class TestPrepareAction(unittest.TestCase):
+    def test_package_list(self):
+        observed = ckanapi.prepare_action('package_list')
+        expected = ('api/action/package_list', '{}', {'Content-Type': 'application/json'})
+        self.assertEqual(observed, expected)
 
 if __name__ == '__main__':
     unittest.main()
