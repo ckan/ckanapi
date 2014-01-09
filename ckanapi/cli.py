4d7cc82eab19548d0b6eb18443e3176435f26ed1
Ian Ward <ian@excess.org>
2014-01-09 15:33:30 -0500

    cli: action command

--- ckanapi/cli.py
+++ ckanapi/cli.py
@@ -35,6 +35,7 @@ Options:
 """
 
 import sys
+import json
 from docopt import docopt
 from pkg_resources import load_entry_point
 
@@ -61,10 +62,42 @@ def main(running_with_paster=False):
     else:
         ckan = LocalCKAN(username=arguments['--ckan-user'])
 
+    if arguments['action']:
+        return action(ckan, arguments)
+
+    load_commands = ['load-datasets', 'load-groups', 'load-organizations']
+    command = [x for x in load_commands if arguments[x]]
+    if command:
+        assert len(command) == 1, command
+        return load_things(ckan, command[0], arguments)
+
+    dump_commands = ['dump-datasets', 'dump-groups', 'dump-organizations']
+    command = [x for x in dump_commands if arguments[x]]
+    if command:
+        assert len(command) == 1, command
+        return dump_things(ckan, command[0], arguments)
+
+    assert 0, arguments # we shouldn't be here
 
 
-    print arguments, ckan
+def action(ckan, arguments):
+    """
+    call an action with KEY=VALUE args, output the result on success
+    """
+    action_args = {}
+    for kv in arguments['KEY=VALUE']:
+        key, p, value = kv.partition('=')
+        action_args[key] = value
+    result = ckan.call_action(arguments['ACTION_NAME'], action_args)
 
+    if arguments['--jsonl']:
+        if isinstance(result, list):
+            for r in result:
+                sys.stdout.write(_compact_json(r) + '\n')
+        else:
+            sys.stdout.write(_compact_json(result) + '\n')
+    else:
+        sys.stdout.write(_pretty_json(result) + '\n')
 
 
 def parse_arguments():
@@ -77,3 +110,11 @@ def _switch_to_paster(arguments):
     """
     sys.argv[1:1] = ["ckanapi"]
     sys.exit(load_entry_point('PasteScript', 'console_scripts', 'paster')())
+
+
+def _compact_json(r):
+    return json.dumps(r, ensure_ascii=False, separators=(',', ':'))
+
+
+def _pretty_json(r):
+    return json.dumps(r, ensure_ascii=False, separators=(',', ': '), indent=2)
