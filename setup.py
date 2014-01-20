6d19548e3527cd88eaa661d8d0c17461e05583d8
Ian Ward <ian@excess.org>
2014-01-20 13:26:40 -0500

    setup.py:license = BSD

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(name='ckanapi',
       version='2.4-dev',
       description='Thin wrapper around the CKAN Action API',
+      license='BSD',
       author='Ian Ward',
       author_email='ian@excess.org',
       url='https://github.com/open-data/ckanapi',
