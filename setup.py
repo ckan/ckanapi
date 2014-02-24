9140479161e3e7bf022ac4201615e4821fb42046
Ian Ward <ian@excess.org>
2014-02-24 09:20:23 -0500

    version 3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ckanapi',
-    version='3.0',
+    version='3.1',
     description=
         'A command line interface and Python module for '
         'accessing the CKAN Action API',
