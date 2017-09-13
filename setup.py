c7229445b58e126204fef9522fccd22b462c86d2
Ian Ward <ian@excess.org>
2017-09-13 11:46:59 -0400

    version 4.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info <= (3,):
 
 setup(
     name='ckanapi',
-    version='4.0',
+    version='4.1',
     description=
         'A command line interface and Python module for '
         'accessing the CKAN Action API',
