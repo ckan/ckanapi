7a4c63e62709037c8cf5d9f7880abaab73dc4f2f
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-12-23 20:22:11 +0000

    feat(dev): package list;
    - More arguments for package list for `--all` dataset dump.

--- ckanapi/cli/dump.py
+++ ckanapi/cli/dump.py
@@ -57,7 +57,10 @@ def dump_things(ckan, thing, arguments,
             }[thing]
         params = dict(
             all_fields=False,  # for user_list
-            )
+            include_private=arguments['--include-private'],  # for package_list
+            include_drafts=arguments['--include-drafts'],  # for package_list
+            include_deleted=arguments['--include-deleted'],  # for package_list
+        )
         names = ckan.call_action(get_thing_list, params)
 
     else:
